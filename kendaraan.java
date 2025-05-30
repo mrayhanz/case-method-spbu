7c33082945d3ed390d2c04edd563071810559c09
Nadyaaurora <nadyaaurora575@gmail.com>
2025-05-30 14:02:47 +0700

    feat : class kendaraan

--- kendaraan.java
+++ kendaraan.java
@@ -1,4 +1,14 @@
 public class kendaraan {
+    String platNomor, tipe, merk;
 
-    
+    public kendaraan(String platNomor, String tipe, String merk) {
+        this.platNomor = platNomor;
+        this.tipe = tipe;
+        this.merk = merk;    
+    }
+    public void tampilkanInformasi() {
+        System.out.println("Plat Nomor : " + platNomor);
+        System.out.println("Tipe       : " + tipe);
+        System.out.println("Merk       : " + merk);
+    }
 }
