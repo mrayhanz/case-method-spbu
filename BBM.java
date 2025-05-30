7a4bc81cb4712aefd1bf2d7f4e6ace59ee30b693
Nadyaaurora <nadyaaurora575@gmail.com>
2025-05-30 14:27:35 +0700

    feat : class bbm

--- BBM.java
+++ BBM.java
@@ -1,3 +1,19 @@
 public class BBM {
-    
+    String namaBBM;
+    double hargaPerLiter;
+
+    public BBM(String namaBBM, double hargaPerLiter) {
+        this.namaBBM = namaBBM;
+        this.hargaPerLiter = hargaPerLiter;
+    }
+    public String getNamaBBM() {
+        return namaBBM;
+    }
+    public double getHargaPerLiter() {
+        return hargaPerLiter;
+    }
+    public void tampilkanInformasi() {
+        System.out.println("Masukkan Jenis BBM       : " + namaBBM);
+        System.out.println("Masukkan Harga per Liter : " + hargaPerLiter);
+    }
 }
